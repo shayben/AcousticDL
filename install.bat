61d225f09657f6b933bfb6a05a321c3fd696bb37
Shay <shaybenelazar@hotmail.com>
2019-06-06 08:44:06 +0300

    no message

---
+++ install.bat
@@ -0,0 +1,3 @@
+pip install https://download.pytorch.org/whl/cpu/torch-1.1.0-cp37-cp37m-win_amd64.whl
+pip install https://download.pytorch.org/whl/cpu/torchvision-0.3.0-cp37-cp37m-win_amd64.whl
+pip install -r requirements.txt
